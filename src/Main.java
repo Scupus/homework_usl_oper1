b1a1573c7521877625f919cfe3cb3f04f9b5f5d2
Alex <loalantref@hmail.com>
2022-11-19 17:36:18 +1200

    Начали

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,35 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+        int age = 12;
+        if (age >= 18) {
+            System.out.println("Поздравляем с совершеннолетием!");
+        }
+        if (age < 18) {
+            System.out.println("Пока что не дорос, малец.");
+        }
+        System.out.println("____________________________________________________________________________");
+        int age2 =19;
+        if (age2 >= 7 && age2 < 18) {
+            System.out.println("Поздравляем с совершеннолетием!");
+        }
+        if (age2 >= 18 && age2 < 24) {
+            System.out.println("Поздравляем с совершеннолетием!");
+        }
+        if (age >= 24) {
+            System.out.println("Поздравляем с совершеннолетием!");
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
 }
